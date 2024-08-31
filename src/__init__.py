b199af2c5c86171ee7098e4fdb969ee9a64bb79e
Rafalfaro <16846380+rafalfaro18@users.noreply.github.com>
2024-08-30 22:06:39 -0600

    Remove .textproto from gitignore

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 import AddressBook_pb2
 import google.protobuf.text_format as text_format
+import os
 
-file_name = "AddressBook.textproto"
+file_name = os.path.join("./tests/", "AddressBook.textproto")
+
 address_book = AddressBook_pb2.AddressBook()
 person = address_book.people.add()
 
@@ -11,6 +13,15 @@ person.email = "example@example.com"
 phone = person.phones.add()
 phone.number = "8888-8888"
 phone.type = AddressBook_pb2.Person.PHONE_TYPE_MOBILE
+
+person2 = address_book.people.add()
+
+person2.id = 2345
+person2.name = "John Doe 2"
+person2.email = "example@example.com"
+phone2 = person2.phones.add()
+phone2.number = "8888-8888"
+phone2.type = AddressBook_pb2.Person.PHONE_TYPE_MOBILE
 
 ab_text = text_format.MessageToString(address_book)
 
