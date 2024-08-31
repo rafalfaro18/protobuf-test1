ca6cc57eb88f8de9467578c7f78261d7402b8f48
Rafalfaro <16846380+rafalfaro18@users.noreply.github.com>
2024-08-30 22:17:41 -0600

    Restore serializing to binary string besides text format

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ import AddressBook_pb2
 import google.protobuf.text_format as text_format
 import os
 
-file_name = os.path.join("./tests/", "AddressBook.textproto")
+file_name_str = os.path.join("./tests/", "AddressBook.textproto")
 
 address_book = AddressBook_pb2.AddressBook()
 person = address_book.people.add()
@@ -25,17 +25,33 @@ phone2.type = AddressBook_pb2.Person.PHONE_TYPE_MOBILE
 
 ab_text = text_format.MessageToString(address_book)
 
-# Save address book
-with open(file_name, "wt") as f:
+# Save address book as text format
+with open(file_name_str, "wt") as f:
   f.write(ab_text)
   f.close()
 
 address_book2 = AddressBook_pb2.AddressBook()
 
-# Read the existing address book.
-with open(file_name, "rt") as f:
+# Read the existing address book from text format.
+with open(file_name_str, "rt") as f:
   ab2_text = f.read()
   text_format.Parse(ab2_text, address_book2)
   f.close()
 
 print(address_book2.people.pop().name)
+
+file_name_bin = os.path.join("./tests/", "AddressBook.binpb")
+
+# Save address book as binary 'Wire' format
+with open(file_name_bin, "wb") as f:
+  f.write(address_book.SerializeToString())
+  f.close()
+
+address_book3 = AddressBook_pb2.AddressBook()
+
+# Read the existing address book from binary 'Wire' format.
+with open(file_name_bin, "rb") as f:
+  address_book3.ParseFromString(f.read())
+  f.close()
+
+print(address_book3.people.pop().name)
